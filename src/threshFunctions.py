f2377fc92ab602b28ac2de8966191d293d7c5619
Kergan Sanderson <kasande4@ncsu.edu>
2024-06-01 19:02:18 -0400

    Fix threshold game of life splicing

--- src/threshFunctions.py
+++ src/threshFunctions.py
@@ -3,26 +3,24 @@ import numpy as np
 # applys a threshold to an image array
 def applyThreshToArr(arr, thresh):
 
-    # these may be turned into buttons later
+    # these may be turned into buttons later for function input
     gOLFactor = 4
     gOLIter = 1
 
+    # apply the initial threshold
     output = arr > thresh
 
-    # TODO fix slicing not working vertically
-    # output[0][:] = 0
-    # output[:][0] = 0
-    # output[len(output) - 1][:] = 0
-    # output[:][len(output) - 1] = 0
+    # set the outsides of the image to False
+    output[0, :] = False
+    output[:, 0] = False
+    output[len(output) - 1, :] = False
+    output[:, len(output[0]) - 1] = False
 
-    return output
-
-    # TODO fix array splicing so that the game of life works
     # play game of life with the thresholded image
-    # temp = np.full(output.shape, True)
-    # for i in range(0, gOLIter):
-    #     for r in range(1, len(output) - 1):
-    #         for c in range(1, len(output[r]) - 1):
-    #             if np.sum(output[r-1:r+2][c-1:c+2]) < gOLFactor:
-    #                 temp[r][c] = False
-    # return temp
+    for i in range(0, gOLIter):
+        for r in range(1, len(output) - 1):
+            for c in range(1, len(output[r]) - 1):
+                if np.sum(output[r-1:r+2, c-1:c+2]) < gOLFactor:
+                    output[r][c] = False
+    
+    return output
